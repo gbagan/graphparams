bf7d4fb62056f6ae5130f9a8c3867d93305f94bd
gbagan <guillaume.bagan@liris.cnrs.fr>
2020-05-22 11:01:35 +0200

    Delete config.js

--- .storybook/config.js
+++
@@ -1,8 +0,0 @@
-import { configure } from '@storybook/react';
-
-// automatically import all files ending in *.stories.js
-function loadStories() {
-  require("../stories/index.tsx");
-}
-
-configure(loadStories, module);
